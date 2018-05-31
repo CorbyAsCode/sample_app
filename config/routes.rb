50147825d004fd6c42b91c0782ba959a146797fc
Corby Shaner <corbyshaner@gmail.com>
2018-05-30 20:58:01 -0400

    Added temporary session cookies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'session#destroy'
+
   resources :users
 
   get 'signup', to: 'users#new'
